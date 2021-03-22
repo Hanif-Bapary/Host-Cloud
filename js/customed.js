3456d9586ad8e59af444787673e6f3e29ea909c9
Hanif Bapary <79474540+Hanif-Bapary@users.noreply.github.com>
2021-03-22 19:45:46 +0600

    Add files via upload

---
+++ js/customed.js
@@ -0,0 +1,11 @@
+$(function(){
+  //  Testimonial..............................
+    $('.testimonial').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow:1,
+        autoplay:true,
+        autoplaySpeed: 2000,
+      }); 
+})
+
